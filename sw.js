e94dbd59538ee32e2edd8361e64a38c7f6b1bcc3
chen10eason <chen10eason@gmail.com>
2026-09-19 22:03:23 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ticketbox-v1.9.15-push-reliability-v5';
+const CACHE='ticketbox-v1.9.15-final';
 const CORE=['./','./index.html'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
